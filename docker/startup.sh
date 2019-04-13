68c1b208e2d80b04b734ed4ed45f1f5b6819f074
Lukas <lukas@infornautik.de>
2019-04-13 16:38:46 +0200

    Updated readme

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -n "$PROXY_API_HOST" ]]; then
-  echo "Seting PROXY_API_HOST=$PROXY_API_HOST"
+  echo "Setting PROXY_API_HOST=$PROXY_API_HOST"
   envsubst '$PROXY_API_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
   export API_HOST="/api"
   echo "Setting API_HOST=$API_HOST"
